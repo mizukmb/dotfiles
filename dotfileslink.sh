4e1042827882c3687abce42c4724cd5020559942
mizukmb <mizukmb6@gmail.com>
2016-05-12 18:01:20 +0900

    add .gitignore

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vimshrc ~/.vimshrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
